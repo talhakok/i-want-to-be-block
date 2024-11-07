576c69d12c7f5373b3b0f0bf94329bf260dc4d61
Talha Kök <talhakokbusiness@gmail.com>
2024-11-07 20:20:46 +0300

    kodları itu blockchain github'ından caldıgım icin utandım

--- solid.sol
+++ solid.sol
@@ -1,22 +1,2 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
-
-import {Test} from "forge-std/Test.sol";
-import {StdInvariant} from "forge-std/StdInvariant.sol";
-import {console} from "forge-std/console.sol";
-
-contract CCIPTokenSenderTest is StdInvariant, Test {
-    
-
-    function setUp() public {
-        
-    }
-
-    function test_() public{
-        
-    }
-
-    function invariant_() public{
-        
-    }
-}
