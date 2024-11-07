eeb4fae1de300a68818c9e6be8b924e8eb7550bc
Talha Kök <talhakokbusiness@gmail.com>
2024-11-07 20:08:40 +0300

    solide kod ekledim

---
+++ solid.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {console} from "forge-std/console.sol";
+
+contract CCIPTokenSenderTest is StdInvariant, Test {
+    
+
+    function setUp() public {
+        
+    }
+
+    function test_() public{
+        
+    }
+
+    function invariant_() public{
+        
+    }
+}
